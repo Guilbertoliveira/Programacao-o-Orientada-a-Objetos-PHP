1c5b211cf1c4309ba19edfe316cdea0e53de8f74
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-03-11 18:23:25 -0300

    atualizando

--- getset/index.php
+++ getset/index.php
@@ -10,8 +10,19 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <pre> <!-- pre formatamento -->
         <?php
-        // put your code here
+        require_once 'Caneta.php';
+        
+        $c1 = new Caneta();
+        $c1->setModelo("Paraguai");
+        $c1->setPonta(0.5);
+       // $c1->ponta = 0.2; erro para demonstrar o private
+       echo "eu tenho uma caneta {$c1->getModelo()} de ponta {$c1->getPonta()}"
+            
+        
         ?>
+        </pre>
+        
     </body>
 </html>
