76d8c642ea02600a8d0facafb1d1cfc8278695bc
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-03-13 17:56:47 -0300

    atualizando

--- Exercicio1/Luta.php
+++ Exercicio1/Luta.php
@@ -1,5 +1,65 @@
 <?php
 
 class Luta {
+   private $desafiado;
+   private $desafiante;
+   private $rounds;
+   private $aprovado;
    
+   
+   public function marcarLuta(){
+       
+   }
+   
+   public function lutar(){
+       
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   function getDesafiado() {
+       return $this->desafiado;
+   }
+
+   function getDesafiante() {
+       return $this->desafiante;
+   }
+
+   function getRounds() {
+       return $this->rounds;
+   }
+
+   function getAprovado() {
+       return $this->aprovado;
+   }
+
+   function setDesafiado($desafiado) {
+       $this->desafiado = $desafiado;
+   }
+
+   function setDesafiante($desafiante) {
+       $this->desafiante = $desafiante;
+   }
+
+   function setRounds($rounds) {
+       $this->rounds = $rounds;
+   }
+
+   function setAprovado($aprovado) {
+       $this->aprovado = $aprovado;
+   }
+
+
 }
