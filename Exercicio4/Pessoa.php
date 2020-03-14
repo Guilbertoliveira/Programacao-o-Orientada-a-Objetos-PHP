4fb02b697e5b91b6eda6a6d76de9f58b75f1b31a
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-03-14 17:14:03 -0300

    atualizado

--- Exercicio4/Pessoa.php
+++ Exercicio4/Pessoa.php
@@ -22,4 +22,55 @@ class Pessoa {
         
     }
     
+    
+    
+    function __construct($nome, $idade, $sexo) {
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->sexo = $sexo;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getIdade() {
+        return $this->idade;
+    }
+
+    function getSexo() {
+        return $this->sexo;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setIdade($idade) {
+        $this->idade = $idade;
+    }
+
+    function setSexo($sexo) {
+        $this->sexo = $sexo;
+    }
+
+
 }
