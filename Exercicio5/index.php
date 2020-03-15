5e0256f5a7d7eb48b16d6e76c4ad1a9b8bedf3a4
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-03-15 15:28:51 -0300

    atualizando

--- Exercicio5/index.php
+++ Exercicio5/index.php
@@ -9,10 +9,29 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
+    <body><pre>
         <?php
 
-        echo "oi";
+        require_once "./Pessoa.php";
+        require_once "./Funcionario.php";
+        require_once './Aluno.php';
+        require_once './Professor.php';
+        
+        $p = new Pessoa();
+        $p1 = new Aluno;
+        $p2 = new Professor();
+        $p3 = new Funcionario();
+        
+        
+        $p->setNome("jose");
+        $p1->setNome("arlindo");
+        $p2->setNome("clerivaldo");
+        $p3->setNome("pedro");
+        
+        print_r($p1);
+        print_r($p);
+        print_r($p2);
+        print_r($p3);
         ?>
-    </body>
+    </body></pre>
 </html>
