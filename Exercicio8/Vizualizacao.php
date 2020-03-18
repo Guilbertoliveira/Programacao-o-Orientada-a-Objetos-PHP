2391ba89d49b16b726e0552522131c024c0bdcc5
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2020-03-18 14:50:05 -0300

    atualizando

---
+++ Exercicio8/Vizualizacao.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Vizualizacao
+ *
+ * @author guilb
+ */
+class Vizualizacao {
+   private $espectador;
+   private $filme;
+   
+   public function avaliar()
+   {
+       
+   }
+   
+   public function avaliarNota($nota)
+   {
+       
+   }
+   
+   public function avaliarPorc($porc)
+   {
+       
+   }
+}
